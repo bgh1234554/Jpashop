13907d80638a950fc53a2e0432c86096d8270406
baekJiHoon <bgh1234554@gmail.com>
2025-05-27 02:48:01 +0900

    20250527 feat: JPA 2편 섹션 4 후반부 자료 보고 필기 완료

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,9 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.Getter;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.*;
 
 /*
 OrderSimpleApiController에서 Order와 OneToMany 관계인
@@ -195,6 +200,7 @@ public class OrderApiController {
     /**
      * V4 - JPA에서 DTO로 직접 조회
      * 컬렉션이 들어갔을 때 쿼리용 DTO 레포를 어떻게 만들 수 있을까?
+     * 실질적인 로직은 order.query 패키지 참조
      */
     private final OrderQueryRepository orderQueryRepository;
 
@@ -202,4 +208,63 @@ public class OrderApiController {
     public List<OrderQueryDto> ordersV4(){
         return orderQueryRepository.findOrderQueryDtos();
     }
+
+    /**
+     * V5 - JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화
+     *
+     * 여러 주문을 한꺼번에 조회하는 경우에는 V4 대신에 V5를 사용하는 것이 좋다.
+     *
+     * 예를 들어서 조회한 Order 데이터가 1000건인데,
+     * V4 방식을 그대로 사용하면, 쿼리가 총 1 + 1000번 실행된다.
+     * 여기서 1은 Order 를 조회한 쿼리고, 1000은 조회된 Order의 row 수다.
+     * V5 방식으로 최적화 하면 쿼리가 총 1 + 1번만 실행된다
+     */
+    @GetMapping("api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    /**
+     * V6 - JPA에서 DTO로 직접 조회 - 플랫 데이터 최적화
+     * JOIN 결과를 그대로 조회한 후에 애플리케이션에서 원하는 모양으로 직접 변환한다.
+     *
+     * 쿼리는 한번이지만 조인으로 인해 DB에서 애플리케이션에 전달하는 데이터에 중복 데이터가 추가되므로
+     * 상황에 따라 V5 보다 더 느릴 수 도 있다. 또한, 페이징이 불가능하다.
+     */
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6(){
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+
+        return flats.stream()
+                .collect(groupingBy(o->new OrderQueryDto(o.getOrderId(),
+                        o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o-> new OrderItemQueryDto(o.getOrderId(), o.getItemName(),
+                                o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e->new OrderQueryDto(
+                        e.getKey().getOrderId(),e.getKey().getName(),e.getKey().getOrderDate(),
+                        e.getKey().getOrderStatus(),e.getKey().getAddress(),e.getValue()
+                )).toList();
+        /*
+        join을 다 걸어서 모든 Order, Member, Delivery, OrderItem, Item 데이터를 **Flat 구조(일자형)**로 가져옴.
+
+        한 방 쿼리로 다 가져온 다음에 애플리케이션에서 Order 기준으로 그룹핑
+         */
+    }
+
+    /*
+    <결론>
+
+    1. 엔티티 조회 방식으로 우선 접근
+        1. 페치조인으로 쿼리 수를 최적화
+        2. 컬렉션 최적화
+            1. 페이징 필요 hibernate.default_batch_fetch_size , @BatchSize 로 최적화
+            2. 페이징 필요X 페치 조인 사용
+    2. 엔티티 조회 방식으로 해결이 안되면 DTO 조회 방식 사용
+    3. DTO 조회 방식으로 해결이 안되면 NativeSQL or 스프링 JdbcTemplate
+
+    엔티티 조회 방식은 JPA가 많은 부분을 최적화 해주기 때문에,
+    단순한 코드를 유지하면서, 성능을 최적화 할 수 있다.
+    반면에 DTO 조회 방식은 SQL을 직접 다루는 것과 유사하기 때문에, 둘 사이에 줄타기를 해야 한다.
+     */
 }
--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -1,13 +1,130 @@
 package jpabook.jpashop.repository.order.query;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderQueryRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 컬렉션은 별도로 조회
+     * Query: 루트 1번, 컬렉션 N 번
+     * 단건 조회에서 많이 사용하는 방식
+     * ToOne 관계는 조인으로 최적화 하기 쉬우므로 한번에 조회하고,
+     * ToMany 관계는 최적화 하기 어려우므로 findOrderItems() 같은 별도의 메서드로 조회한다.
+     *
+     * DTO가 두개 필요하니까 각각 쿼리용 DTO를 만들고, 각각 최적화한 이후에 합쳐서 조회한다.
+     */
     public List<OrderQueryDto> findOrderQueryDtos() {
+        //toOne 코드를 한번에 조회
+        List<OrderQueryDto> result = findOrders();
+
+        //각 OrderQueryDto에 OrderItemQueryDto를 추가
+        //forEach문을 돌 때마다 새로운 쿼리가 나가게 된다.
+        result.forEach(o->{
+            List<OrderItemQueryDto> orderItems = findOrderItems(o.getOrderId());
+            o.setOrderItems(orderItems);
+        });
+        return result;
+    }
+
+    /**
+     * orderItems 빼고 조회
+     */
+    private List<OrderQueryDto> findOrders() {
+        return em.createQuery("select new" +
+                        " jpabook.jpashop.repository.order.query.OrderQueryDto" +
+                "(o.id, m.name, o.orderDate, o.status, d.address)" +
+                " from Order o" +
+                " join o.member m" +
+                " join o.delivery d", OrderQueryDto.class)
+                .getResultList();
+    }
+
+    /**
+     * orderItems 조회
+     */
+    private List<OrderItemQueryDto> findOrderItems(Long orderId){
+        return em.createQuery(
+                "select new" +
+                        " jpabook.jpashop.repository.order.query.OrderItemQueryDto" +
+                        "(oi.order.id, i.name, oi.orderPrice, oi.count)"+
+                        " from OrderItem oi"+
+                        " join oi.item i"+
+                        " where oi.order.id = :orderId",
+                OrderItemQueryDto.class)
+                .setParameter("orderId",orderId)
+                .getResultList();
+    }
+
+    /**
+     * 최적화 버전 (V5)
+     * Query는 order에 대해 한번, 컬렉션에 대해 한번
+     * 데이터를 한꺼번에 처리할 때 사용하는 방식이다.
+     */
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        //toOne 코드를 한번에 조회
+        List<OrderQueryDto> result = findOrders();
+
+        //이후 해당 주문의 ID에 대한
+        //orderItem 컬렉션을 MAP을 이용해 조회후
+        Map<Long, List<OrderItemQueryDto>> orderItemMap
+                = findOrderItemMap(toOrderIds(result));
+
+        //루프를 돌면서 컬렉션 추가
+        result.forEach(o->o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+    }
+
+    /*
+    특정 주문들(orderIds)에 해당하는 모든 OrderItem을 "한 번에 조회"해서
+    주문 ID를 키로, 해당하는 OrderItemQueryDto 리스트를 값으로 가지는 Map으로 구성
+     */
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        //JPQL로 모든 주문 아이템을 한번에 조회해서 OrderItemQueryDto에 담는다.
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto"+
+                        "(oi.order.id, i.name, oi.orderPrice, oi.count)"+
+                        " from OrderItem oi" +
+                        " join oi.item i"+
+                        //in이 들어가서 한번에 모든 주문 ID에 대한 orderItem 들을 조회할 수 있게 된다.
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+        //Collectors.groupingBy(Function keyMapper)
+        //List를 Map으로 바꿔주게 된다.
+        //즉, 주문 ID별로 주문 상품들을 묶어주는 역할을 한다.
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    //OrderQueryDto의 리스트에서 orderId만 뽑아서 리스트로 반환
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                .map(OrderQueryDto::getOrderId)
+                .toList();
+    }
+
+    /**
+     * 플랫 데이터 버전 (V6)
+     */
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderFlatDto(" +
+                        "o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)"+
+                        " from Order o"+
+                        " join o.member m"+
+                        " join o.delivery d"+
+                        " join o.orderItems oi"+
+                        " join oi.item i", OrderFlatDto.class)
+                .getResultList();
     }
 }
